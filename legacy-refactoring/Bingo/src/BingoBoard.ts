bdff30865f4220d25d339159586b665380714e22
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-23 09:17:03 +0000

    refactor classes

--- legacy-refactoring/Bingo/src/BingoBoard.ts
+++ legacy-refactoring/Bingo/src/BingoBoard.ts
@@ -1,4 +1,5 @@
 import { Cell } from "./Cell";
+import { Coordinate } from "./Coordinate";
 import { Game } from "./Game";
 
 export class BingoBoard extends Game<string> {
@@ -17,29 +18,34 @@ export class BingoBoard extends Game<string> {
     );
   }
 
-  defineCell(x: number, y: number, value: string): void {
+  defineCell(coordinate: Coordinate, value: string): void {
+    const { x, y } = coordinate;
     const cell = this.cells[x][y];
     if (cell?.getValue() !== undefined) {
       throw new Error("cell already defined");
     }
-    for (let c = 0; c < this.cells.length; c++) {
-      for (let r = 0; r < this.cells[c].length; r++) {
-        if (value === this.cells[c][r]?.getValue()) {
-          throw new Error(`${value} already present at ${c},${r}`);
+
+    for (let row = 0; row < this.cells.length; row++) {
+      for (let column = 0; column < this.cells[row].length; column++) {
+        if (value === this.cells[row][column]?.getValue()) {
+          throw new Error(`${value} already present at ${row},${column}`);
         }
       }
     }
+
     cell?.define(value);
   }
 
-  markCell(x: number, y: number): void {
+  markCell(coordinate: Coordinate): void {
+    const { x, y } = coordinate;
     if (!this.isInitialized()) {
       throw new Error("board not initialized");
     }
     this.marked[x][y] = true;
   }
 
-  isMarked(x: number, y: number): boolean {
+  isMarked(coordinate: Coordinate): boolean {
+    const { x, y } = coordinate;
     return this.marked[x][y];
   }
 

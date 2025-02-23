2e6c4a8c14950f05038925304204eb4b7f5b36de
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-23 12:33:10 +0000

    remove sanity checkers

--- legacy-refactoring/Bingo/test/bingoboard.test.ts
+++ legacy-refactoring/Bingo/test/bingoboard.test.ts
@@ -7,7 +7,6 @@ describe("BingoBoard", () => {
   beforeEach(() => {
     board = new BingoBoard(2, 2);
 
-    // This makes sese, we are creating a simple board with 4 cells
     board.defineCell(new Coordinate(0, 0), "10");
     board.defineCell(new Coordinate(0, 1), "20");
     board.defineCell(new Coordinate(1, 0), "30");
@@ -15,29 +14,23 @@ describe("BingoBoard", () => {
   });
 
   afterEach(() => {
-    // This also makes sense, we are resetting the board after each test
     board = new BingoBoard(2, 2);
   });
 
   describe("initialization", () => {
     it("should throw error if cell is already defined", () => {
-      //This makes sense, we are trying to define a cell that is already defined
-      // It was already set up in the beforeEach
       expect(() => board.defineCell(new Coordinate(0, 0), "10")).toThrow(
         "cell already defined"
       );
     });
 
     it("should initialize board after all cells are defined", () => {
-      // This makes sense, we are checking if the board is initialized
-      // It was already set up in the beforeEach
       expect(board.isInitialized()).toBe(true);
     });
   });
 
   describe("cell marking", () => {
     it("should mark a cell", () => {
-      // This makes sense, we are marking a cell
       board.markCell(new Coordinate(0, 0));
       expect(board.isMarked(new Coordinate(0, 0))).toBe(true);
     });

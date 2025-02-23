ffbe2629c2c89cd65934dba7efc12344ea2bb813
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-02-23 08:47:31 +0000

    now using cell class

--- legacy-refactoring/Bingo/src/BingoBoard.ts
+++ legacy-refactoring/Bingo/src/BingoBoard.ts
@@ -1,29 +1,35 @@
+import { Cell } from "./Cell";
 import { Game } from "./Game";
 
 export class BingoBoard extends Game<string> {
-  private cells: string[][];
+  private cells: Array<Array<Cell<string> | null>>;
   private marked: boolean[][];
 
   constructor(width: number, height: number) {
     super();
-    this.cells = Array.from({ length: width }, () => Array(height).fill(null));
+    this.cells = Array.from({ length: width }, () =>
+      Array(height)
+        .fill(null)
+        .map(() => new Cell<string>())
+    );
     this.marked = Array.from({ length: width }, () =>
       Array(height).fill(false)
     );
   }
 
   defineCell(x: number, y: number, value: string): void {
-    if (this.cells[x][y] !== null) {
+    const cell = this.cells[x][y];
+    if (cell?.getValue() !== undefined) {
       throw new Error("cell already defined");
     }
     for (let c = 0; c < this.cells.length; c++) {
       for (let r = 0; r < this.cells[c].length; r++) {
-        if (value === this.cells[c][r]) {
+        if (value === this.cells[c][r]?.getValue()) {
           throw new Error(`${value} already present at ${c},${r}`);
         }
       }
     }
-    this.cells[x][y] = value;
+    cell?.define(value);
   }
 
   markCell(x: number, y: number): void {
